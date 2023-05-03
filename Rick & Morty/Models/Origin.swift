2e3a24577c56dbbc6794ea42c40870845cd6f524
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-05-03 23:57:32 +0300

    Added models

---
+++ Rick & Morty/Models/Origin.swift
@@ -0,0 +1,13 @@
+//
+//  Origin.swift
+//  Rick & Morty
+//
+//  Created by Николай Никитин on 03.05.2023.
+//
+
+import Foundation
+
+struct Origin: Codable {
+  let name: String
+  let url: String
+}
---
+++ Rick & Morty/Models/SingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  SingleLocation.swift
+//  Rick & Morty
+//
+//  Created by Николай Никитин on 03.05.2023.
+//
+
+import Foundation
+
+struct SingleLocation: Codable {
+  let name: String
+  let url: String
+}
