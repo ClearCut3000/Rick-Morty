ad5b2ea200ee856766a76a17608a4c2edd769652
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-05-09 12:45:25 +0300

    Character List View

--- Rick & Morty/ViewModels/CharacterListViewViewModel.swift
+++ Rick & Morty/ViewModels/CharacterListViewViewModel.swift
@@ -5,10 +5,37 @@
 //  Created by Николай Никитин on 08.05.2023.
 //
 
-import Foundation
+import UIKit
 
-struct CharacterListViewViewModel {
+final class CharacterListViewViewModel: NSObject {
   func fetchCharacters() {
     
+  }
+}
+
+// MARK: - CollectionView DataSource Protocol Extension
+extension CharacterListViewViewModel: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 20
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    cell.backgroundColor = .cyan
+    return cell
+  }
+}
+
+//MARK: - CollectionView Delegate Protocol Extension
+extension CharacterListViewViewModel: UICollectionViewDelegate {
+
+}
+
+//MARK: - CollectionView FlowLayout Protocol Extension
+extension CharacterListViewViewModel: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let bounds = UIScreen.main.bounds
+    let width = (bounds.width - 30) / 2
+    return CGSize(width: width, height: width * 1.5)
   }
 }
--- Rick & Morty/Views/CharacterListView.swift
+++ Rick & Morty/Views/CharacterListView.swift
@@ -18,14 +18,26 @@ final class CharacterListView: UIView {
     spinner.translatesAutoresizingMaskIntoConstraints = false
     return spinner
   }()
+  private let collectionView: UICollectionView = {
+    let layout = UICollectionViewFlowLayout()
+    layout.scrollDirection = .vertical
+    layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    collectionView.isHidden = true
+    collectionView.alpha = 0
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    return collectionView
+  }()
 
   // MARK: - Init's
   override init(frame: CGRect) {
     super.init(frame: frame)
     translatesAutoresizingMaskIntoConstraints = false
-    addSubview(spinner)
+    addSubviews(collectionView, spinner)
     addConstraints()
     spinner.startAnimating()
+    setupCollectionView()
   }
 
   required init?(coder: NSCoder) {
@@ -38,8 +50,17 @@ final class CharacterListView: UIView {
       spinner.widthAnchor.constraint(equalToConstant: 100),
       spinner.heightAnchor.constraint(equalToConstant: 100),
       spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
-      spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+      spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+      collectionView.topAnchor.constraint(equalTo: topAnchor),
+      collectionView.leftAnchor.constraint(equalTo: leftAnchor),
+      collectionView.rightAnchor.constraint(equalTo: rightAnchor),
+      collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
     ])
   }
 
+  private func setupCollectionView() {
+    collectionView.dataSource = viewModel
+    collectionView.delegate = viewModel
+  }
 }
