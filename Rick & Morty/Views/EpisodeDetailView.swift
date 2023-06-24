25eef1a661cbd8313bdde8ed9d90c2e7780a9dea
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-06-24 23:58:17 +0300

    Episode Detail Layout

--- Rick & Morty/Views/EpisodeDetailView.swift
+++ Rick & Morty/Views/EpisodeDetailView.swift
@@ -41,13 +41,29 @@ final class EpisodeDetailView: UIView {
   }
 
   private func createCollectionView() -> UICollectionView {
-
-    return UICollectionView()
+    let layout = UICollectionViewCompositionalLayout { section, _ in
+      return self.layout(for: section)
+    }
+    let collectionView = UICollectionView(frame: .zero,
+                                          collectionViewLayout: layout)
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    return collectionView
   }
 
   public func configure(with viewModel: EpisodeDetailViewViewModel) {
     self.viewModel = viewModel
   }
+}
 
-
+// MARK: - Layout Extension
+extension EpisodeDetailView {
+  private func layout(for section: Int) -> NSCollectionLayoutSection {
+    let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1),
+                                                        heightDimension: .fractionalHeight(1)))
+    let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(widthDimension: .fractionalWidth(1),
+                                                                   heightDimension: .fractionalHeight(1)),
+                                                 subitems: [item])
+    let section = NSCollectionLayoutSection(group: group)
+    return section
+  }
 }
